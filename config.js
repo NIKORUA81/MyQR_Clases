8f50ae497416d187ebe29d1fcf07889e769b1b53
Nicolás Rúa <60671846+NIKORUA81@users.noreply.github.com>
2025-02-26 16:09:58 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // MODIFICA ESTA URL POR LA DEL SERVICIO WEB DESPUES DE HABERLO DESPLEGADO
-const AS_URL_BASE = "https://script.google.com/macros/s/AquiVaTuURL/exec";
+const AS_URL_BASE = "https://script.google.com/macros/s/AKfycbyi1XDbooRjhG_LxZdHsYM0dXihmOhEgR1qtt3UguBh8pzUEF7BAUbP3I4A_iXRH4ig/exec";
